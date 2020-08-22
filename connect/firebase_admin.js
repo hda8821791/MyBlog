34b29acea3551644509dded8f07d9a771b1eb115
金中騰 <hda8821791@gmail.com>
2020-08-22 14:21:06 +0800

    update

--- connect/firebase_admin.js
+++ connect/firebase_admin.js
@@ -1,11 +1,21 @@
-var admin = require("firebase-admin");
-
-var serviceAccount = require("../myprojectjs-41393-firebase-adminsdk-83y2f-cb5a81d704.json");
+const admin = require("firebase-admin");
+require('dotenv').config();
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://myprojectjs-41393.firebaseio.com",
+    credential: admin.credential.cert({
+        type: 'service_account',
+        project_id: process.env.FIREBASE_PROJECT_ID,
+        private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+        private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        client_email: process.env.FIREBASE_CLIENT_EMAIL,
+        client_id: process.env.FIREBASE_CLIENT_ID,
+        auth_uri: process.env.FIREBASE_AUTH_URI,
+        token_uri: process.env.FIREBASE_TOKEN_URI,
+        auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+        client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    }),
+    databaseURL: process.env.FIREBASE_DATABASEURL,
 });
 
-var db = admin.database();
+const db = admin.database();
 module.exports = db;
